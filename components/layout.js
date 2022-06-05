6319cf4d01e57b61746f4e7cde27f327db05d789
tamusan100 <tamusan100@gmail.com>
2022-06-05 17:53:58 +0900

    タイトル変更とお試し記事作成

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'tamu'
+const name = 'Nagato Design'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
@@ -28,30 +28,10 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/">
                 <a className={utilStyles.colorInherit}>{name}</a>
---
+++ posts/utility-tools.md
@@ -0,0 +1,7 @@
+---
+title: '便利ツールまとめ'
+date: '2022-06-05'
+---
+
+- [Color Hunt](https://colorhunt.co/)
+  - お気に入りの配色パターンを探せる
